c362c1777b3f2da2ca9ee5a150c939e4c16fe75e
abdallaheldoud <abdallah.abbas.jr@gmail.com>
2023-09-14 14:51:46 -0700

    5th

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,33 +1,24 @@
+#!/usr/bin/python3
+'''2D matrix'''
+
+
 def rotate_2d_matrix(matrix):
-    """
-    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
-    """
-    n = len(matrix)
-    for layer in range(n // 2):
-        first, last = layer, n - 1 - layer
-        for i in range(first, last):
-            # Save the top element
-            top = matrix[first][i]
+    '''rotates a 2d matrix 90° clockwise
+    Returns: Nothing '''
+    left, right = 0, len(matrix) - 1
 
-            # Move left to top
-            matrix[first][i] = matrix[last - (i - first)][first]
-
-            # Move bottom to left
-            matrix[last - (i - first)][first] = matrix[last][last - (i - first)]
-
-            # Move right to bottom
-            matrix[last][last - (i - first)] = matrix[i][last]
-
-            # Move top to right
-            matrix[i][last] = top
-
-# Testing the function
-if __name__ == "__main__":
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-
-    rotate_2d_matrix(matrix)
-    for row in matrix:
-        print(row)
-
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            # save topleft  value
+            topLeft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
